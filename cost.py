28738f0a4ed89529a6a64184e9d1356c0ba977af
Breenda Das <breenda.das23@gmail.com>
2024-08-15 11:20:28 +0200

    Update cost.py

--- cost.py
+++ cost.py
@@ -1,7 +1,6 @@
-from encoder import FeatureEncoder
-
 import torch
-
+import torch.nn as nn
+from encoder import FeatureEncoder
 
 class CostEstimator(torch.nn.Module):
     refine_steps = 50
@@ -11,6 +10,7 @@ class CostEstimator(torch.nn.Module):
         super().__init__()
         self.device = device
         self.encoder = FeatureEncoder(mlp_out=1).to(self.device)  # Move the encoder to the specified device
+        self.prelu = nn.PReLU().to(self.device)  # Define PReLU layer and move it to the specified device
 
     def fit_pipeline(self, config, curve, budget, target, meta_feat):
         optimizer = torch.optim.Adam(self.parameters(), self.refine_lr)
@@ -22,23 +22,22 @@ class CostEstimator(torch.nn.Module):
 
         for i in range(self.refine_steps):
             optimizer.zero_grad()
+
             # Move input tensors to the correct device
-            feat = self.encoder(config, curve, budget, meta_feat).to(self.device)
-            output = torch.nn.functional.relu(feat)
+            output = self.encoder(config, curve, budget, meta_feat).to(self.device)
+
             loss = criterion(output, target.float())
             loss.backward()
             optimizer.step()
             print("Step: {} Loss: {}".format(i, loss.item()))
 
-    def predict_pipeline(
-        self,
-        config, curve, budget, meta_feat
-    ):
+    def predict_pipeline(self, config, curve, budget, meta_feat):
         self.eval()
 
         with torch.no_grad():
             # Move input tensors to the correct device
-            feat = self.encoder(config, curve, budget, meta_feat).to(self.device)
-            output = torch.nn.functional.relu(feat)
+            output = self.encoder(config, curve, budget, meta_feat).to(self.device)
+
+            # Apply PReLU activation
 
         return output
